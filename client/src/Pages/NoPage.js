46b6ed10dd551861d050c8d53ae4a6f66abe4110
ArturBobotcko <artur.bobotcko@mail.ru>
2024-04-24 20:32:14 +0300

    Made 404 page

--- client/src/Pages/NoPage.js
+++ client/src/Pages/NoPage.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const NoPage = () => {
   return (
-    <h2>Error 404: Not found</h2>
-  )
-}
+    <div class="d-flex align-items-center justify-content-center vh-100">
+      <div class="text-center">
+        <h1 class="display-1 fw-bold">404</h1>
+        <p class="fs-3">
+          {" "}
+          <span class="text-danger">Упс!</span> Страница не найдена.
+        </p>
+        <p class="lead">Страница, которую вы ищете не существует.</p>
+        <a href="/" class="btn btn-primary">
+          Главная
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default NoPage
+export default NoPage;
