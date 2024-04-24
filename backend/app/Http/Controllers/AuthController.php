b0610c8486eec04dcf9b9c6e5c6cbd87887437e3
ArturBobotcko <artur.bobotcko@mail.ru>
2024-04-24 19:12:23 +0300

    Realised profile and logout api functions

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -105,4 +105,25 @@ class AuthController extends Controller
             ], 500);
         }
     }
+
+    public function profile()
+    {
+        $userData = auth()->user();
+        return response()->json([
+            'status' => true,
+            'message' => 'Profile information',
+            'data' => $userData,
+            'id' => auth()->user()->id
+        ], 200);
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'User logged out',
+            'data' => [],
+        ], 200);
+    }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,6 +7,13 @@ use App\Http\Controllers\AuthController;
 Route::post("register", [AuthController::class, 'register']);
 Route::post("login", [AuthController::class, 'login']);
 
+Route::group([
+    "middleware" => ["auth:sanctum"]
+], function(){
+    Route::get("profile", [AuthController::class, 'profile']);
+    Route::get("logout", [AuthController::class, 'logout']);
+});
+
 /*Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');*/
